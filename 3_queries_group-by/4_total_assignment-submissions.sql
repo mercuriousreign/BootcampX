e53b94dc7d13d3a3a5f89b27a6398702ab902b0e
Zaima Atoshi <zaimaz.atoshi@gmail.com>
2023-02-09 12:25:39 -0500

    Add queries that calculate average completion and total assignment submissions

--- 3_queries_group-by/4_total_assignment-submissions.sql
+++ 3_queries_group-by/4_total_assignment-submissions.sql
@@ -6,14 +6,3 @@ JOIN cohorts
 ON cohorts.id = students.cohort_id
 GROUP BY cohorts.name
 ORDER BY total_submissions DESC;
-
-
-
-
--- SELECT cohorts.name, count (*) AS student_count 
--- FROM students
--- JOIN cohorts 
--- ON cohorts.id = students.cohort_id
--- GROUP BY cohorts.name
--- ORDER BY student_count;
- 
